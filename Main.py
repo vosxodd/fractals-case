63fe64501f1507cab9960d658902146de5e47601
vosxodd <73089079+vosxodd@users.noreply.github.com>
2021-04-14 10:24:02 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,8 +3,6 @@
 #               Soloveychik D. (%),
 #               Labuzov A. (%)
 
-
-
 # нужно сделать следующие фракталы: 1) бинарное дерево, 2) ветка, 3) кривая Коха (сделана в задании), 4) Кривая Минковского, 5) Ледяные фракталы, 
 # 6) Кривая Леви, 7) Фрактал Дракон Хартера-Хейтуэя
 # ледяной фрактал Андрей Геннадиевич рекомендовал построить каждому
@@ -12,7 +10,7 @@
 # один из вариантов улучшения кода - добавление смены цвета во фракталах. Ляпота
 # плюсом в работе будет рисование снежинок у некоторых фракталов
 
-def ice(order, size):#ледяной фрактал
+def ice(order, size): # ледяной фрактал
     if order == 0:          
         forward(size)
     else:
@@ -23,11 +21,11 @@ def ice(order, size):#ледяной фрактал
         ice(order-1, size/4)
         left(90)
         ice(order-1, size/2)
-def branch(n, size):#ветка
+        
+def branch(n, size): # ветка
     if n == 0:
         left(180)
         return
-
     x = size/(n+1)
     for i in range(n):
         forward(x)
@@ -36,11 +34,11 @@ def branch(n, size):#ветка
         left(90)
         branch(n-i-1, 0.5*x*(n-i-1))
         right(135)
-
     forward(x)
     left(180)
     forward(size)
-def levi(order, size):#фрактал Леви
+    
+def levi(order, size): # фрактал Леви
     if order == 0:          
         forward(size)
     else:
@@ -49,4 +47,23 @@ def levi(order, size):#фрактал Леви
         right(90)
         levi(order-1, size)
         left(45)
+        
+def mincovski(order, size): # Кривая Минковского
+    if order == 0:
+        forward(size)
+    else:
+        mincovski(order - 1, size / 4)
+        left(90)
+        mincovski(order - 1, size / 4)
+        right(90)
+        mincovski(order - 1, size / 4)
+        right(90)
+        mincovski(order - 1, size / 4)
+        mincovski(order - 1, size / 4)
+        left(90)
+        mincovski(order - 1, size / 4)
+        left(90)
+        mincovski(order - 1, size / 4)
+        right(90)
+        mincovski(order - 1, size / 4)
 
